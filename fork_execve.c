d2e37ad5e8c8207a244d764de55cfa61986b4bc0
Mark Waweru <markwawerumwai@gmail.com>
2023-11-15 18:03:29 -0800

    final touches

--- fork_execve.c
+++ fork_execve.c
@@ -9,11 +9,13 @@
  */
 void fork_execve(char *inputStr)
 {
-	pid_t cmdChild;
+	pid_t cmdChild, childPid;
 	extern char **environ;
 	char *token;
 	char *args[10];
 	int argCount = 0;
+	char *customPath;
+	char **env;
 
 	token = strtok(inputStr, " ");
 	while (token != NULL)
@@ -22,25 +24,68 @@ void fork_execve(char *inputStr)
 		token = strtok(NULL, " ");
 	}
 	args[argCount] = NULL;
-
-	cmdChild = fork();
-	if (cmdChild == -1)
+	if (strcmp(args[0], "env") == 0)
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-
-	if (cmdChild == 0)
-	{
-		if (execve(args[0], args, environ) == -1)
+		for (env = environ; *env != NULL; env++)
 		{
-			printf("No such file or directory\n");
-			exit(EXIT_FAILURE);
+			printf("%s\n", *env);
 		}
-		free(inputStr);
+	}
+	else if (strchr(args[0], '/') == NULL)
+	{
+		customPath = "usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin";
+		setenv("PATH", customPath, 1);
+		if (access(args[0], X_OK) == 0)
+		{
+			childPid = fork();
+
+			if (childPid == -1)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+
+			if (childPid == 0)
+			{
+				if (execve(args[0], args, environ))
+				{
+					perror("execve");
+					exit(EXIT_FAILURE);
+				}
+				free(inputStr);
+			}
+			else
+			{
+				my_wait(childPid, inputStr);
+			}
+	
+		}
+		else
+		{
+			printf("Command not found:\n");
+		}
 	}
 	else
 	{
-	my_wait(cmdChild, inputStr);
+		cmdChild = fork();
+		if (cmdChild == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+
+		if (cmdChild == 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				printf("No such file or directory\n");
+				exit(EXIT_FAILURE);
+			}
+			free(inputStr);
+		}
+		else
+		{
+			my_wait(cmdChild, inputStr);
+		}
 	}
 }
